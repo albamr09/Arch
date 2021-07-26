194d067a22b72dea7ac4fd6504e81db27c9596a2
albamr09 <87711484+albamr09@users.noreply.github.com>
2021-07-26 11:32:20 +0200

    Instalar yay en lugar de yaourt

--- src/utils/yaourt.sh
+++ src/utils/yaourt.sh
@@ -1,29 +1,38 @@
 
 #!/bin/bash
 
-# dependencias
+#--------------------------------------------------------
+#----------------------  YAOURT  ------------------------
+#--------------------------------------------------------
 
-sudo pacman -S --needed base-devel git wget yajl
+# Dependencias yaourt
+
+#sudo pacman -S --needed base-devel git wget yajl
 
 
 # package-query
 
-cd /tmp
-git clone https://aur.archlinux.org/package-query.git
-cd package-query/
-makepkg -si && cd /tmp/
+#cd /tmp
+#git clone https://aur.archlinux.org/package-query.git
+#cd package-query/
+#makepkg -si && cd /tmp/
 
 # yaourt
 
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt/
-makepkg -si
-
-# dependencias yay
-
-#sudo pacman -S --needed base-devel git
-
-#cd /tmp 
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
+#git clone https://aur.archlinux.org/yaourt.git
+#cd yaourt/
 #makepkg -si
+
+#--------------------------------------------------------
+#------------------------  YAY  -------------------------
+#--------------------------------------------------------
+
+# Dependencias yay
+
+sudo pacman -S --needed base-devel git
+
+# Instalar yay
+cd /tmp 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
