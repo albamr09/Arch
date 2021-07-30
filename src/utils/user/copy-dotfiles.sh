84704e81d255a4fce6347495aec9de0b90946bdc
albamr99 <alba.martinez.rodriguez@rai.usc.es>
2021-07-30 17:37:48 +0200

    Arreglo en copia usuario

--- src/utils/user/copy-dotfiles.sh
+++ src/utils/user/copy-dotfiles.sh
@@ -11,5 +11,6 @@ sudo cp -r $DIR_DOTFILES/.??* /home/$USUARIO &> /dev/zero && mensaje_exito "Se h
 
 # Cambiar owner y permisos
 
-sudo chown -R $USUARIO /home/$USUARIO/.??* && chmod -R 775 /home/$USUARIO/.??* &> /dev/zero && mensaje_exito "Se han cambiado los permisos para $USUARIO" || mensaje_fallo "Fallo durante el cambio de permisos en $USUARIO"
-sudo chown -R $USUARIO /home/$USUARIO/* && chmod -R 775 /home/$USUARIO/??* &> /dev/zero && mensaje_exito "Se han cambiado los permisos para $USUARIO" || mensaje_fallo "Fallo durante el cambio de permisos en $USUARIO"
+sudo chown -R $USUARIO /home/$USUARIO/.??* && chmod -R 665 /home/$USUARIO/.??* &> /dev/zero && mensaje_exito "Se han cambiado los permisos para $USUARIO" || mensaje_fallo "Fallo durante el cambio de permisos en $USUARIO"
+sudo chown -R $USUARIO /home/$USUARIO/* && chmod -R 665 /home/$USUARIO/??* &> /dev/zero && mensaje_exito "Se han cambiado los permisos para $USUARIO" || mensaje_fallo "Fallo durante el cambio de permisos en $USUARIO"
+chmod 775 /home/$USUARIO/.xsession &> /dev/zero && mensaje_exito "Se han cambiado los permisos para $USUARIO de xsession" || mensaje_fallo "Fallo durante el cambio de permisos en $USUARIO se xsession"
