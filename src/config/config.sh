ec0386877963bc446c02882b636e230f41ae47d9
albamr09 <87711484+albamr09@users.noreply.github.com>
2021-08-04 12:44:10 +0200

    Instalar gpicview

--- src/config/config.sh
+++ src/config/config.sh
@@ -32,7 +32,7 @@ DIRECTORIO_GRUB_CONF="/boot/grub/grub.cfg"
 
 TERMINAL="alacritty"
 TWM="i3-wm i3-gaps i3status"
-PAQUETES="nitrogen vim git wget neovim python-pip zsh nano texlive-most xdg-utils qutebrowser zathura zathura-pdf-poppler ranger w3m cmus"
+PAQUETES="nitrogen vim git wget neovim python-pip zsh nano texlive-most xdg-utils qutebrowser zathura zathura-pdf-poppler ranger w3m cmus gpicview"
 #thunar
 PAQUETES_AUR="vscode;pamixer;pacman-contrib;ttf-font-awesome;foxitreader"
 #surf;firefox;
