e6c7692a1346a1bf49a892abcf9dd0fc23f3b0ba
albamr09 <87711484+albamr09@users.noreply.github.com>
2021-08-06 17:30:52 +0200

    [i3 status] Cambiar colores

--- resources/config-files/dotfiles/.config/i3status/mybar.sh
+++ resources/config-files/dotfiles/.config/i3status/mybar.sh
@@ -5,14 +5,14 @@
 #   status_command exec /home/alba/.config/i3status/mybar.sh
 # }
 
-bg_bar_color="#282A36"
+bg_bar_color="#181a1c"
 
 # Print a left caret separator
 # @params {string} $1 text color, ex: "#FF0000"
 # @params {string} $2 background color, ex: "#FF0000"
 separator() {
   echo -n "{"
-  echo -n "\"full_text\":\"\"," # CTRL+Ue0b2
+  echo -n "\"full_text\":\"\"," # CTRL+Ue0b2
   echo -n "\"separator\":false,"
   echo -n "\"separator_block_width\":0,"
   echo -n "\"border\":\"$bg_bar_color\","
@@ -36,11 +36,12 @@ common() {
 }
 
 mycrypto() {
-  local bg="#FFD180"
+  local bg="#d4879c"
   separator $bg $bg_bar_color
   echo -n ",{"
   echo -n "\"name\":\"id_crypto\","
-  echo -n "\"full_text\":\" $(/home/alba/.config/i3status/crypto.py) \","
+  echo -n "\"full_text\":\"  \","
+	#echo -n "\"full_text\":\"     $(/home/alba/.config/i3status/crypto.py)     \","
   echo -n "\"color\":\"#000000\","
   echo -n "\"background\":\"$bg\","
   common
@@ -48,35 +49,37 @@ mycrypto() {
 }
 
 myip_public() {
-  local bg="#1976D2"
-  separator $bg "#FFD180"
+  local bg="#454a4f"
+  separator $bg "#d4879c"
   echo -n ",{"
   echo -n "\"name\":\"ip_public\","
-  echo -n "\"full_text\":\" $(/home/alba/.config/i3status/ip.py) \","
+	echo -n "\"full_text\":\"  \","
+  #echo -n "\"full_text\":\" $(/home/alba/.config/i3status/ip.py) \","
   echo -n "\"background\":\"$bg\","
   common
   echo -n "},"
 }
 
 myvpn_on() {
-  local bg="#424242" # grey darken-3
+  local bg="#b5bd68" # grey darken-3
   local icon=""
   if [ -d /proc/sys/net/ipv4/conf/proton0 ]; then
     bg="#E53935" # rouge
     icon=""
   fi
-  separator $bg "#1976D2" # background left previous block
+  separator $bg "#454a4f" # background left previous block
   bg_separator_previous=$bg
   echo -n ",{"
   echo -n "\"name\":\"id_vpn\","      
   echo -n "\"full_text\":\" ${icon} VPN \","
+	echo -n "\"color\":\"#000000\","
   echo -n "\"background\":\"$bg\","
   common
   echo -n "},"
 }
 
 myip_local() {
-  local bg="#2E7D32" # vert
+  local bg="#282a2e" # vert
   separator $bg $bg_separator_previous
   echo -n ",{"
   echo -n "\"name\":\"ip_local\","
@@ -87,12 +90,13 @@ myip_local() {
 }
 
 disk_usage() {
-  local bg="#3949AB"
-  separator $bg "#2E7D32"
+  local bg="#fb4731"
+  separator $bg "#282a2e"
   echo -n ",{"
   echo -n "\"name\":\"id_disk_usage\","
   echo -n "\"full_text\":\"  $(/home/alba/.config/i3status/disk.py)%\","
   echo -n "\"background\":\"$bg\","
+	echo -n "\"color\":\"#000000\","
   common
   echo -n "}"
 }
@@ -101,7 +105,8 @@ memory() {
   echo -n ",{"
   echo -n "\"name\":\"id_memory\","
   echo -n "\"full_text\":\"  $(/home/alba/.config/i3status/memory.py)%\","
-  echo -n "\"background\":\"#3949AB\","
+  echo -n "\"background\":\"#fb4731\","
+	echo -n "\"color\":\"#000000\","
   common
   echo -n "}"
 }
@@ -110,14 +115,15 @@ cpu_usage() {
   echo -n ",{"
   echo -n "\"name\":\"id_cpu_usage\","
   echo -n "\"full_text\":\"  $(/home/alba/.config/i3status/cpu.py)% \","
-  echo -n "\"background\":\"#3949AB\","
+  echo -n "\"background\":\"#fb4731\","
+	echo -n "\"color\":\"#000000\","
   common
   echo -n "},"
 }
 
 meteo() {
-  local bg="#546E7A"
-  separator $bg "#3949AB"
+  local bg="#454a4f"
+  separator $bg "#fb4731"
   echo -n ",{"
   echo -n "\"name\":\"id_meteo\","
   echo -n "\"full_text\":\" $(/home/alba/.config/i3status/meteo.py) \","
@@ -127,8 +133,8 @@ meteo() {
 }
 
 mydate() {
-  local bg="#E0E0E0"
-  separator $bg "#546E7A"
+  local bg="#fabe2b"
+  separator $bg "#454a4f"
   echo -n ",{"
   echo -n "\"name\":\"id_time\","
   echo -n "\"full_text\":\"  $(date "+%a %d/%m %H:%M") \","
@@ -140,8 +146,8 @@ mydate() {
 
 battery0() {
   if [ -f /sys/class/power_supply/BAT0/uevent ]; then
-    local bg="#D69E2E"
-    separator $bg "#E0E0E0"
+    local bg="#282a2e"
+    separator $bg "#fabe2b"
     bg_separator_previous=$bg
     prct=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=" | cut -d'=' -f2)
     charging=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_STATUS" | cut -d'=' -f2) # POWER_SUPPLY_STATUS=Discharging|Charging
@@ -152,7 +158,6 @@ battery0() {
     echo -n ",{"
     echo -n "\"name\":\"battery0\","
     echo -n "\"full_text\":\" ${icon} ${prct}% \","
-    echo -n "\"color\":\"#000000\","
     echo -n "\"background\":\"$bg\","
     common
     echo -n "},"
@@ -162,7 +167,7 @@ battery0() {
 }
 
 volume() {
-  local bg="#673AB7"
+  local bg="#508c8c"
   separator $bg $bg_separator_previous  
   vol=$(awk '/%/ {gsub(/[\[\]]/,""); print $4}' <(amixer sget Master))
   echo -n ",{"
@@ -173,6 +178,7 @@ volume() {
     echo -n "\"full_text\":\"  ${vol} \","
   fi
   echo -n "\"background\":\"$bg\","
+	echo -n "\"color\":\"#000000\","
   common
   echo -n "},"
   separator $bg_bar_color $bg
@@ -248,8 +254,8 @@ do
     xdg-open https://openweathermap.org/city/2986140 > /dev/null &
 
   # CRYPTO
-  elif [[ $line == *"name"*"id_crypto"* ]]; then
-    xdg-open https://www.livecoinwatch.com/ > /dev/null &
+  #elif [[ $line == *"name"*"id_crypto"* ]]; then
+  #  xdg-open https://www.livecoinwatch.com/ > /dev/null &
 
   # VOLUME
   elif [[ $line == *"name"*"id_volume"* ]]; then
