e5d623e425a0757725476521a1110b8717c5a23d
albamr09 <bertaalba1@gmail.com>
2021-08-08 23:14:25 +0200

    Añadir fuente Jetbrains

--- scripts/connect_usb.sh
+++ scripts/connect_usb.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 # Crear directorio temporal y montar usb
-mkdir /tmp/SANDISK/ && sudo mount /dev/sdc1 /tmp/SANDISK -o uid=alba
+# - $1: dispositivo a montar
+# - $2: directorio
+# - $3: usuario
+
+DISPOSITIVO=$1
+DIRECTORIO=$2
+USUARIO=$3
+
+if [ -z "$1" ]
+  then
+    DISPOSITIVO="sdc1"
+fi
+
+if [ -z "$2" ]
+  then
+    DIRECTORIO="/tmp/SANDISK"
+fi
+
+if [ -z "$3" ]
+  then
+	USUARIO="alba"
+fi
+
+mkdir $DIRECTORIO && sudo mount /dev/$DISPOSITIVO $DIRECTORIO -o uid=$USUARIO
