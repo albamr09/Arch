ae383cb7481c9fdecd2e562c024fd60baaa874b8
albamr09 <bertaalba1@gmail.com>
2021-08-09 00:13:50 +0200

    [qutebrowser] Temas

---
+++ resources/config-files/dotfiles/.config/qutebrowser/config.py
@@ -0,0 +1,13 @@
+config.source('themes/gruvbox.py')
+
+#import dracula.draw
+
+# Load existing settings made via :set
+#config.load_autoconfig()
+
+#dracula.draw.blood(c, {
+#    'spacing': {
+#        'vertical': 3,
+#        'horizontal': 3
+#    }
+#})
