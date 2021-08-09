d82b8c638266f8f94f119464c38343047abbfdd4
albamr09 <bertaalba1@gmail.com>
2021-08-09 10:57:50 +0200

    [qutebrowser] Cambiar config (autoconfig)

--- resources/config-files/dotfiles/.config/qutebrowser/config.py
+++ resources/config-files/dotfiles/.config/qutebrowser/config.py
@@ -1,10 +1,7 @@
+config.load_autoconfig()
 config.source('themes/gruvbox.py')
 
 #import dracula.draw
-
-# Load existing settings made via :set
-#config.load_autoconfig()
-
 #dracula.draw.blood(c, {
 #    'spacing': {
 #        'vertical': 3,
