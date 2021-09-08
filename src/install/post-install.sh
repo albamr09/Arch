1ca3ff055da7ed9bb5e6d96ed92d664b0522b855
Alba Martínez Rodríguez <alba.martinez.rodriguez@rai.usc.es>
2021-09-08 10:09:54 +0200

    [resources] Change i3status.config directory and add lightdm conf

--- src/install/post-install.sh
+++ src/install/post-install.sh
@@ -163,7 +163,7 @@ copiar_dotfiles(){
   echo "----------------------------------------------"
 
   cp -r $DIR_DOTFILES/.??* ~ &> /dev/zero && mensaje_exito "Se han copiado los ficheros de configuracion para root" || mensaje_fallo "Fallo durante la copia de los ficheros de configuracion en root"
-  cp $DIR_RESOURCES/etc/i3status.conf /etc &> /dev/zero && mensaje_exito "Se ha copiado configuracion i3status" || mensaje_fallo "Fallo durante la copia de la configuracion de i3status"
+  cp $DIR_RESOURCES/etc/* /etc &> /dev/zero && mensaje_exito "Se ha copiado directorio etc" || mensaje_fallo "Fallo durante la copia del directorio etc"
 
   # Copia usuario
   chmod +x $DIR_USER_SCRIPTS"/"$COPY_DOTFILES
