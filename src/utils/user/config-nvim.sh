750c90fe16cc9fdd2aa5d4c9194f78e234a28785
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-07-16 13:08:56 +0200

    [nvim] Add clang linting and formatting

--- src/utils/user/config-nvim.sh
+++ src/utils/user/config-nvim.sh
@@ -3,4 +3,4 @@
 curl -fLo /home/$USER/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c 'so ~/.config/nvim/init.vim|q'
 nvim -c 'PlugInstall|q|q|q'
-pip3 install neovim
+pip3 install neovim cpplint
