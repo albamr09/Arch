2d179994574e0914aa845a0b9c8a8c763a16a78d
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-09-17 15:05:44 +0200

    Add debugger to nvim

--- src/utils/user/config-nvim.sh
+++ src/utils/user/config-nvim.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Virtual environments for python
+wd=$(pwd)
+mkdir -p /home/$USER/.virtualenvs && cd /home/$USER/.virtualenvs
+python -m venv debugpy
+cd $wd
+
 curl -fLo /home/$USER/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -c 'PlugInstall|q|q'
 nvim -c 'so ~/.config/nvim/init.vim|q'
 nvim -c 'PlugInstall|q|q|q'
 pip3 install neovim cpplint pynvim
