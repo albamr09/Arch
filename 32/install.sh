45e98a1c18d561f0df734edcab48bebd571c5f45
albamr09 <bertaalba1@gmail.com>
2023-07-29 13:40:25 +0200

    Test system setup

--- 32/install.sh
+++ 32/install.sh
@@ -97,16 +97,10 @@ installing_firmware(){
 
 system_configuration(){
 
-    title_msg "System configuration"
+    title_msg "System configuration and setup"
 
     cp -rf $WORKDIR /mnt && log "Creating install folder on mnt"
-    arch-chroot /mnt /bin/bash -c "cd ./$INSTALL_FOLDER/chroot/ && ./system_config.sh" && log "Performing chroot on system config"
-}
-
-system_setup() {
-
-    title_msg "System setup"
-
+    # arch-chroot /mnt /bin/bash -c "cd ./$INSTALL_FOLDER/chroot/ && ./system_config.sh" && log "Performing chroot on system config"
     arch-chroot /mnt /bin/bash -c "cd ./$INSTALL_FOLDER/chroot/ && ./system_setup.sh" && log "Performing chroot on system setup"
 }
 
@@ -129,5 +123,4 @@ copy_dotfiles(){
 # 5
 # system_configuration
 # 6
-system_setup
 # copy_dotfiles
