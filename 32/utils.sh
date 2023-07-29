620c8ee16d29eacc4804ed928bb6fbf2a0957d5b
albamr09 <bertaalba1@gmail.com>
2023-07-29 11:33:08 +0200

    Fix colors and add bold

--- 32/utils.sh
+++ 32/utils.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 # ANSI color codes
+BLACK='\033[0;30m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+BOLD='\033[1m'
 RESET='\033[0m'
 
 function echo_with_color {
     local text=$1
     local color=$2
-    echo -e "${color}${text}${RESET}"
+    local style=$3
+
+    local format=""
+    if [ "$style" = "bold" ]; then
+        format="${BOLD}"
+    fi
+
+    echo -e "${format}${color}${text}${RESET}"
 }
 
 title_msg() {
-    echo_with_color "----------------------------------------------" $BLUE
-    echo_with_color " + $1" $BLUE
-    echo_with_color "----------------------------------------------" $BLUE
+    echo_with_color "----------------------------------------------" $CYAN bold
+    echo_with_color " + $1" $CYAN bold
+    echo_with_color "----------------------------------------------" $CYAN bold
 }
 
 log() {
