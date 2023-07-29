3ad5bdbdc6c3a746387fb4f0ff4352a9a73d029b
albamr09 <bertaalba1@gmail.com>
2023-07-29 21:10:37 +0200

    Remove spaces

--- install_scripts/post_install.sh
+++ install_scripts/post_install.sh
@@ -16,19 +16,17 @@ configure_services(){
     title_msg "Configuring services"
     systemctl enable lightdm.service
 
-
-	execute sudo cp $DIR_SERVICES/* /etc/systemd/system
-	execute sudo systemctl enable suspend@$USER
-	# Battery notifcation service
+    execute sudo cp $DIR_SERVICES/* /etc/systemd/system
+    execute sudo systemctl enable suspend@$USER
+    # Battery notifcation service
     execute su -c "systemctl --user enable check-battery-user.timer" $USER
     execute su -c "systemctl --user start check-battery-user.service" $USER
-	execute sudo systemctl daemon-reload
+    execute sudo systemctl daemon-reload
 }
 
 cleanup() {
 
     title_msg "Removing installation files"
-
     execute cd / && rm -r $INSTALL_FOLDER
 }
 
