6f817badecfbc82097d8ee0e3f190a078dc5d1c2
albamr09 <bertaalba1@gmail.com>
2024-06-16 15:07:33 +0200

    Rename

---
+++ themes/Cottage/home/.config/qutebrowser/config.py
@@ -0,0 +1,5 @@
+config.load_autoconfig()
+c.url.searchengines = {'DEFAULT': 'https://searx.xyz/search?q={}'}
+c.url.default_page = 'https://searx.xyz/'
+c.url.start_pages = ['https://searx.xyz/']
+config.source('themes/nord-qutebrowser.py')
