a187cfc1a66c0afe61035dd19e1d5adae0d6663f
albamr09 <bertaalba1@gmail.com>
2025-05-04 08:36:48 +0200

    Improve check battery script

--- themes/common/theme/dotfiles/.config/scripts/battery/check-battery.sh
+++ themes/common/theme/dotfiles/.config/scripts/battery/check-battery.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-BATTINFO=`acpi -b | sed  /"Unknown"/d`
-PRINTINFO=`echo $BATTINFO | cut --fields=3,4 -d " " | awk '{ print substr( $0, 1, length($0)-1 ) }'`
-PERCERTAGE=`echo $BATTINFO | cut -f 2 -d "," | awk '{ print substr( $0, 1, length($0)-1 ) }'`
-
-if [[ `echo $BATTINFO | grep Discharging` && $PERCERTAGE -lt 15 ]] ; then
-       /usr/bin/notify-send -u critical "battery" "$PRINTINFO"
-     elif [[ (`echo $BATTINFO | grep Charging` || `echo $BATTINFO | grep Full`) && $PERCERTAGE -gt 95 ]] ; then
-       /usr/bin/notify-send -u normal "battery" "Fully charged!"
+# Select last line from battery info
+BATTINFO=`acpi -b | sed  /"Unknown"/d | tail -n 1`
+# Extract number value of remaning battery
+PERCENTAGE=$(echo "$BATTINFO" | grep -oP 'Discharging, \K[0-9]+')
+ 
+if [[ `echo $BATTINFO | grep Discharging` && $PERCENTAGE -lt 15 ]] ; then
+  PRINTINFO="Discharging "$PERCENTAGE"%"
+  /usr/bin/notify-send -u critical "battery" "$PRINTINFO"
+elif [[ (`echo $BATTINFO | grep Charging` || `echo $BATTINFO | grep Full`) && $PERCENTAGE -gt 95 ]] ; then
+  /usr/bin/notify-send -u normal "battery" "Fully charged!"
 fi
